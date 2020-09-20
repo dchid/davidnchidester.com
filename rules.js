3db1ab90bea77dedbf1252ff7f8196bd179d2ac8
David Chidester <31967957+david-chidester@users.noreply.github.com>
2020-09-19 19:30:40 -0700

    Moved footer icon html to rules.js

--- rules.js
+++ rules.js
@@ -6,6 +6,11 @@ $(document).ready(function () {
     $('div.hidden').fadeIn(1000).removeClass('hidden');
     //Add the asthetic line to terminal sections
     $("div.start_terminal").append("<span>dchidester@blog:~ ./start_blog.sh <b style=\"font-weight: bolder;\"<b class=\"blink\">_</b></span>");
+    const mailToIcon = "<a href=\"mailto:davidchidester@protonmail.com\"><i class=\"fa fa-envelope\"></i></a>"
+    const linkedInIcon = "<a href=\"https://www.linkedin.com/in/david-chidester/\"><i class=\"fa fa-linkedin\"></i></a>"
+    const gitHubIcon = "<a href=\"https://github.com/david-chidester\"><i class=\"fa fa-github\"></i></a>"
+    const footerIconsHTML = mailToIcon + linkedInIcon + gitHubIcon
+    $("div.footer_icons").append(footerIconsHTML);
     //to control toggling of slidable elements
     $(".toggle_slide_control").click(function(){
         $(".toggle_slide_target").slideToggle("fast");
