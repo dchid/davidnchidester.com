0c321768148f44990ed22bc2f2d03b2d700ac0d6
David Chidester <davidchidester@protonmail.com>
2021-04-28 14:58:08 -0700

    changed github link

--- rules.js
+++ rules.js
@@ -33,7 +33,7 @@ function adaptiveNavBar() {
 function buildFooterHTML(){
     const mailToIcon = "<a href=\"mailto:davidchidester@protonmail.com\" target=\"blank\"><i class=\"fa fa-envelope\"></i></a>";
     const linkedInIcon = "<a href=\"https://www.linkedin.com/in/david-chidester/\" target=\"blank\"><i class=\"fa fa-linkedin\"></i></a>";
-    const gitHubIcon = "<a href=\"https://github.com/david-chidester\" target=\"blank\"><i class=\"fa fa-github\"></i></a>";
+    const gitHubIcon = "<a href=\"https://github.com/dchid\" target=\"blank\"><i class=\"fa fa-github\"></i></a>";
     const instagramIcon = "<a href=\"https://www.instagram.com/fude_dude/\" target=\"blank\"><i class=\"fa fa-instagram\"></i></a>";
     var curr = new Date().getFullYear();
     const copyright = "<p class=\"copyright\"><b>&#169; 2019-"+ curr + " David Chidester. All rights reserved.</b></p>";
