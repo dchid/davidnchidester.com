75f68829a8f37b40e177d83827a0ca4cc546fd15
David Chidester <davidchidester@protonmail.com>
2025-03-02 09:11:21 -0800

    refactor doc

--- posts.js
+++ posts.js
@@ -9,25 +9,26 @@ class BlogPost {
   }
 }
 
+//posts.json temporarily hardcoded
+const posts = [
+  new BlogPost("Preserving Gaming History With The PS-Placable", 3, 2, 2025, "Game_Preservation", true),
+  new BlogPost("Why Jenkins isn't the right tool", 6, 27, 2023, "Jenkins", true),
+  new BlogPost("Host your own server: part 2", 9, 2, 2022, "Personal_Server_2", true),
+  new BlogPost("Arbitrary Precision Arithmetic", 5, 27, 2021, "Arbitrary_Precision", true),
+  new BlogPost("Interview with former Intel and NeXT executive Susan Rockrise", 2, 13, 2021, "Susan_Rockrise", false),
+  new BlogPost("Host Your Own Server!", 1, 17, 2021, "Personal_Server", true),
+  new BlogPost("The power of arm unleashed!", 11, 15, 2020, "arm_unleashed", true),
+  new BlogPost("RISC vs CISC architecures?", 8, 30, 2020, "RISC_vs_CISC", true),
+  new BlogPost("What’s a tiling window manager and why might you use one?", 3, 2, 2020, "tiling_wm", true)
+];
+
 $(document).ready(function () {
   //include blog posts
-  const blogHTML = insertBlogPosts();
+  const blogHTML = insertBlogPosts(posts);
   $("#blogContent").append(blogHTML);
 });
 
-function insertBlogPosts() {
-  //posts.json temporarily hardcoded
-  const posts = [
-    new BlogPost("Preserving Gaming History With The PS-Placable", 3, 2, 2025, "Game_Preservation", true),
-    new BlogPost("Why Jenkins isn't the right tool", 6, 27, 2023, "Jenkins", true),
-    new BlogPost("Host your own server: part 2", 9, 2, 2022, "Personal_Server_2", true),
-    new BlogPost("Arbitrary Precision Arithmetic", 5, 27, 2021, "Arbitrary_Precision", true),
-    new BlogPost("Interview with former Intel and NeXT executive Susan Rockrise", 2, 13, 2021, "Susan_Rockrise", false),
-    new BlogPost("Host Your Own Server!", 1, 17, 2021, "Personal_Server", true),
-    new BlogPost("The power of arm unleashed!", 11, 15, 2020, "arm_unleashed", true),
-    new BlogPost("RISC vs CISC architecures?", 8, 30, 2020, "RISC_vs_CISC", true),
-    new BlogPost("What’s a tiling window manager and why might you use one?", 3, 2, 2020, "tiling_wm", true)
-  ];
+function insertBlogPosts(posts) {
   var htmlString = "";
   for (var i = 0; i < posts.length; i++) {
     //date of blog post
