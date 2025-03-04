a6a4a45328f92e09298588500f3d25f4d0b46fdf
David Chidester <davidchidester@protonmail.com>
2025-03-04 15:08:13 -0800

    update

--- posts.js
+++ posts.js
@@ -1,15 +1,15 @@
 class BlogPost {
   constructor(title, month, day, year, fname, isText) {
     this.title = title;
-    this.day = day;
-    this.month = month;
-    this.year = year;
+    this.day = Number(day);
+    this.month = Number(month);
+    this.year = Number(year);
     this.fname = `blog_post_${[month, day, year].join("-")}_${fname}.html`;
-    this.isText = isText;
+    this.isText = Boolean(isText);
   }
 
   getDate() {
-    return [this.month, this.day, this.year].join("/")
+    return [this.month, this.day, this.year].join("/");
   }
 }
 
@@ -18,26 +18,16 @@ async function getPosts() {
   const data = await rawData.json();
   let postList = []
   data.posts.forEach(post => {
-    postList.append(new BlogPost(post.title, post.month, post.year, post.fname, post.isText));
+    postList.push(new BlogPost(post.title, post.month, post.day, post.year, post.fname, post.isText));
   });
   return postList;
 }
 
-const posts = [
-  new BlogPost("Preserving Gaming History With The PS-Placable", 3, 2, 2025, "Game_Preservation", true),
-  new BlogPost("Why Jenkins isn't the right tool", 6, 27, 2023, "Jenkins", true),
-  new BlogPost("Host your own server: part 2", 9, 2, 2022, "Personal_Server_2", true),
-  new BlogPost("Arbitrary Precision Arithmetic", 5, 27, 2021, "Arbitrary_Precision", true),
-  new BlogPost("Interview with former Intel and NeXT executive Susan Rockrise", 2, 13, 2021, "Susan_Rockrise", false),
-  new BlogPost("Host Your Own Server!", 1, 17, 2021, "Personal_Server", true),
-  new BlogPost("The power of arm unleashed!", 11, 15, 2020, "arm_unleashed", true),
-  new BlogPost("RISC vs CISC architecures?", 8, 30, 2020, "RISC_vs_CISC", true),
-  new BlogPost("What’s a tiling window manager and why might you use one?", 3, 2, 2020, "tiling_wm", true)
-];
-
 $(document).ready(function () {
-  const blogHTML = insertBlogPosts(posts);
-  $("#blogContent").append(blogHTML);
+  getPosts().then(res => {
+    const blogHTML = insertBlogPosts(res);
+    $("#blogContent").append(blogHTML);
+  });
 });
 
 function insertBlogPosts(posts) {
